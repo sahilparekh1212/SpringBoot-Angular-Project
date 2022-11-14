a13fedac67ef8488f6882c1fbb46867433119b2a
Sahil <sahilparekh1212@gmail.com>
2022-11-13 19:05:00 -0500

    Made Update Team form a Reactive Form

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamService } from './services/team.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddTeamComponent } from './add-team/add-team.component';
 import { HomeComponent } from './home/home.component';
 import { UpdateTeamComponent } from './update-team/update-team.component';
@@ -22,6 +22,7 @@ import { UpdateTeamComponent } from './update-team/update-team.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [TeamService],
   bootstrap: [AppComponent]
--- frontend/frontend/src/app/update-team/update-team.component.ts
+++ frontend/frontend/src/app/update-team/update-team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TeamService } from '../services/team.service';
 import { Team } from '../utility/class/team/team';
@@ -13,6 +14,11 @@ export class UpdateTeamComponent implements OnInit {
   team: Team = new Team();
   showMessage: boolean = false;
   isTeamUpdated: boolean = false;
+  updateTeamFormGroup: FormGroup = new FormGroup({
+    teamName: new FormControl(this.team.teamName, Validators.compose([Validators.required, Validators.minLength(3)])),
+    gameName: new FormControl(this.team.gameName, Validators.compose([Validators.required, Validators.minLength(4)])),
+    emailId: new FormControl(this.team.emailId, Validators.compose([Validators.required]))
+  });
 
   constructor(private teamService: TeamService, private activatedRoute: ActivatedRoute) { }
 
@@ -21,13 +27,18 @@ export class UpdateTeamComponent implements OnInit {
   }
 
   onSubmit() {
-    this.updateTeam(this.team);
+    if (this.updateTeamFormGroup.valid) {
+      this.updateTeam(this.team);
+    } else {
+      console.log('Form is invalid');
+    }
   }
 
   getTeam(id: number) {
     this.teamService.getTeam(id).subscribe((res) => {
       if (res) {
         this.team = res;
+        this.defaultFormValues(res);
       } else {
         console.log('Something went wrong -> getTeam() -> res=', res);
       }
@@ -50,6 +61,35 @@ export class UpdateTeamComponent implements OnInit {
       (error) => {
         console.log('Error Occured -> updateTeam() -> error=', error);
       });
+  }
+
+  defaultFormValues(team: Team) {
+    this.updateTeamFormGroup.patchValue({
+      teamName: team.teamName,
+      gameName: team.gameName,
+      emailId: team.emailId
+    });
+  }
+
+  get teamName() {
+    return this.updateTeamFormGroup.get('teamName');
+  }
+
+  get gameName() {
+    return this.updateTeamFormGroup.get('gameName');
+  }
+
+  get emailId() {
+    return this.updateTeamFormGroup.get('emailId');
+  }
+
+  clearForm() {
+    let team = new Team();
+    team.id = this.team.id;
+    team.teamName = '';
+    team.gameName = '';
+    team.emailId = '';
+    this.defaultFormValues(team);
   }
 
 }
