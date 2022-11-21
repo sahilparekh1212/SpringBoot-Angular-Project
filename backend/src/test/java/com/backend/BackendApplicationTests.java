d1f78b34278ca3a0ed293dd15e0e365133fec79b
Sahil <sahilparekh1212@gmail.com>
2022-11-20 22:41:52 -0500

    Added Test Files

--- backend/src/test/java/com/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/BackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 class BackendApplicationTests {
 
 	@Test
