af0ce4fb4821e67bb4a9bc7fa186be3c71085f82
= <sahiparekh1212@gmail.com>
2023-08-29 21:14:24 -0400

    1)Corrected Backend Structure 2)Updated Controllers 3)Updated Readme.md 4) Cleanup

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO teams(team_name,game_name,email_id) VALUES('A','Game A','a@game.com'); 
+INSERT INTO teams(team_name,game_name,email_id) VALUES('B','Game B','b@game.com'); 
+INSERT INTO teams(team_name,game_name,email_id) VALUES('C','Game C','c@game.com'); 
---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE teams (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    team_name VARCHAR(30) NOT NULL,
+    game_name VARCHAR(30) NOT NULL,
+    email_id VARCHAR(30)
+);
